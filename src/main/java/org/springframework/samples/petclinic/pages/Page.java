79b70591cd3cb9d21756d0d6c4f78b0b9c9ea579
MONERAHS <sweet-roz2010@hotmail.com>
2020-05-12 02:39:11 +0300

    Page Class Modification

--- src/main/java/org/springframework/samples/petclinic/pages/Page.java
+++ src/main/java/org/springframework/samples/petclinic/pages/Page.java
@@ -6,34 +6,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-<<<<<<< HEAD
-=======
-
 import java.util.ArrayList;
->>>>>>> master
 import java.util.List;
 import java.util.logging.Level;
 import static java.util.logging.Logger.getLogger;
 import static org.openqa.selenium.By.cssSelector;
-<<<<<<< HEAD
-=======
 import static org.openqa.selenium.By.id;
->>>>>>> master
 
-public class Page {
+public class Page  {
     static {
         //System.setProperty("webdriver.chrome.driver", "/Users/chromedriver");
         System.setProperty("webdriver.chrome.silentOutput", "true");
         getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
     }
 
-
     private final WebDriver driver;
-
     protected Page(WebDriver driver) {
-        this.driver = driver;
-
-    }
+        this.driver = driver; }
 
 
     protected void goTo(String url) {
@@ -45,8 +34,6 @@ public class Page {
         driver.findElement(By.xpath(path)).click();
     }
 
-<<<<<<< HEAD
-=======
     protected void remove(String id) {
         final WebElement element = waitFor(id);
         element.clear();
@@ -99,7 +86,6 @@ public class Page {
         return (exists(path1) && exists(path2));
     }
 
->>>>>>> master
     protected String getText(String cssPath) {
         return driver.findElement(By.xpath(cssPath)).getText();
     }
@@ -114,26 +100,11 @@ public class Page {
         new Select(driver.findElement(By.id(id))).selectByIndex(1);
     }
 
-<<<<<<< HEAD
-    protected void fillDate(String path, String value) {
-        driver.findElement(cssSelector(path)).sendKeys(value);
-    }
-
-//    protected void delete(String path) {
-//        driver.findElement(cssSelector(path)).sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-//    }
-
-    protected void editdelete(String path) {
-        driver.findElement(By.cssSelector(path)).clear();
-        driver.findElement(By.cssSelector(path)).sendKeys("a");
-        driver.findElement(By.cssSelector(path)).sendKeys("\b");
-=======
     protected void select(String id, int i) {
         new Select(driver.findElement(id(id))).selectByIndex(1);
     }
     public boolean isCurrent(String url) {
         return url.equals(driver.getCurrentUrl());
->>>>>>> master
     }
 
 
@@ -155,11 +126,6 @@ public class Page {
         return (new WebDriverWait(driver, waitInterval)).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
     }
 
-<<<<<<< HEAD
-
-    protected boolean exists(String id) {
-        return driver.findElement(By.id(id)) != null;
-=======
     public List<String> allPets(){
         List<WebElement> allPets = driver.findElements(By.xpath("/html/body/app-root/app-owner-detail/div/div/table[2]/tr/app-pet-list/table/tr/td[1]/dl/dd[1]"));
         List<String> petsNames=new ArrayList<>();
@@ -168,7 +134,6 @@ public class Page {
             petsNames.add(pet.getText());
         }
         return petsNames;
->>>>>>> master
     }
 
     public void fillInPetForm(String name, String birthDate, int typeIndex) {
@@ -178,33 +143,11 @@ public class Page {
         select("type",typeIndex);
     }
 
-<<<<<<< HEAD
-
-    protected boolean elementDoesExist (String path) {
-        List<WebElement> dynamicElement = driver.findElements(By.xpath(path));
-        return dynamicElement.size() < dynamicElement.size() + 1;
-    }
-
-
-    protected boolean removeItem (String path){
-=======
     protected boolean removeItem(String path){
->>>>>>> master
         List<WebElement> RemovedElement = driver.findElements(By.xpath(path));
-        return RemovedElement.size() > RemovedElement.size() - 1;
-    }
 
-
-<<<<<<< HEAD
-
-    public boolean isCurrent(String url) {
-        return url.equals(driver.getCurrentUrl());
-=======
         return RemovedElement.size() > RemovedElement.size()-1;
->>>>>>> master
     }
 
-//
 
 }
-
